716385d23834760933647d709a6566331dea114f
zhangzchwn <1064494254@qq.com>
2020-08-27 22:32:33 +0800

    mvvm-2

--- kvue/01-defProp.js
+++ kvue/01-defProp.js
@@ -1,7 +1,13 @@
+/* 1----- */
+/*
 // 响应式
 const obj = {}
+*/
 
 function defineReactive(obj,key,val) {
+    // 递归
+    observe(val);
+
     // 对传入的 obj 进行访问的拦截
     Object.defineProperty(obj,key,{
         get() {
@@ -17,8 +23,35 @@ function defineReactive(obj,key,val) {
     })
 }
 
+function observe(obj) {
+    if (typeof obj !== 'object' || obj == null) {
+        // 希望传入的是obj
+        return
+    }
+
+    Object.keys(obj).forEach(key => {
+        defineReactive(obj, key, obj[key])
+    })
+}
+
+/*
+* 不需要*/
+/*
 defineReactive(obj, 'foo', 'foo')
 obj.foo
 obj.foo = 'fooooooooooooooooo'
+*/
+/* 2------ */
+// 响应式
+const obj = { foo: 'foo', bar: 'bar', baz: { a: 1 }, arr: [1,2,3] }
+
+// 遍历做响应化处理
+observe(obj)
+obj.foo
+obj.foo = 'fooooooooooooooo'
+obj.bar
+obj.bar = 'barrrrrrrrrrrrrr'
+// obj.baz.a = 10 // no ok
+/* 2------ */
 
 
